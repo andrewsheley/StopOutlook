480036b8ebeb94d83f16bed3d526606cc116e06c
Andrew Sheley KDOR <Andrew.Sheley@kdor.ks.gov>
2018-07-06 08:43:20 -0500

    Add project files.

---
+++ StopOutlook/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StopOutlook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var outLooks = Process.GetProcessesByName("outlook");
+
+                outLooks?[0].Kill();
+
+
+            }
+            catch 
+            {
+
+                throw;
+            }
+
+
+            Console.WriteLine("Outlook Stopped");
+        }
+    }
+}
